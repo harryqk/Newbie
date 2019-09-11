1352dcc3100cd1a961f29dd86b43f558ff8ec229
HarryQMac <harryqk@hotmail.com>
2019-09-11 09:00:57 +0800

    update
    update

---
+++ Socket/Assets/Script/SocketClient.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+public class SocketClient
+{
+    string serverIp = "";
+    int serverPort = 0;
+    Socket socketClient = null;
+    Queue<string> queueRead = new Queue<string>();
+    Queue<byte[]> queueMove = new Queue<byte[]>();
+    Queue<string> queueLog = new Queue<string>();
+    byte[] buffer = new byte[1024 * 8];
+    Thread threadConnect = null;
+    Thread threadRead = null;
+    bool heartBreak = false;
+    Thread threadHeartBreak = null;
+    System.Timers.Timer timerSend = null;
+    System.Timers.Timer timerCheck = null;
+    EventHandler onConnectHandler = null;
+    /// <summary>
+    /// 连接服务器
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <param name="port"></param>
+    public void Connect(string ip, int port)
+    {
+        serverIp = ip;
+        serverPort = port;
+        StartConnet();
+    }
+
+    public Queue<string> getReadMsg()
+    {
+        return queueRead;
+    }
+
+    public Queue<byte[]> getMoveMsg()
+    {
+        return queueMove;
+    }
+
+    public Queue<string> GetLog()
+    {
+        return queueLog;
+    }
+
+    /// <summary>
+    /// 关闭socket
+    /// </summary>
+    public void Close()
+    {
+        if (timerSend != null)
+        {
+            timerSend.Close();
+        }
+        if (socketClient != null)
+        {
+            socketClient.Close();
+            socketClient = null;
+        }
+        if (threadRead != null)
+        {
+            threadRead.Abort();
+        }
+        if (threadConnect != null)
+        {
+            threadConnect.Abort();
+        }
+        if (threadHeartBreak != null)
+        {
+            threadHeartBreak.Abort();
+        }
+    }
+
+    /// <summary>
+    /// 开启连接线程
+    /// </summary>
+    void StartConnet()
+    {
+        threadConnect = new Thread(ThreadConnect);
+        threadConnect.IsBackground = true;
+        threadConnect.Start();
+    }
+
+    /// <summary>
+    /// 连接线程
+    /// </summary>
+    void ThreadConnect()
+    {
+        IPAddress ipAddress = IPAddress.Parse(serverIp);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, serverPort);
+        socketClient = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        socketClient.Connect(ipAddress, serverPort);
+        OnConnected();
+    }
+
+    /// <summary>
+    /// 连接成功
+    /// </summary>
+    void OnConnected()
+    {
+        queueLog.Enqueue("连接服务器成功");
+        Receive();
+        queueLog.Enqueue("开始接受数据");
+    }
+
+    /// <summary>
+    /// 开始接收数据
+    /// </summary>
+    void StartReceive()
+    {
+        threadRead = new Thread(Receive);
+        threadRead.IsBackground = true;
+        threadRead.Start();
+    }
+
+    /// <summary>
+    /// 传输数据
+    /// </summary>
+    /// <param name="protocol"></param>
+    /// <param name="s"></param>
+    public void ClientWrite(int protocol, string s)
+    {
+        byte[] dataSend = SocketUtil.ConvertStrToSend(protocol, s);
+        writeByte(dataSend);
+    }
+
+    public void writeByte(byte[] data)
+    {
+        if (data == null
+            && data.Length == 0)
+        {
+            return;
+        }
+        if (socketClient != null
+                && socketClient.Connected == true)
+        {
+            if (socketClient.Poll(-1, SelectMode.SelectWrite))
+            {
+                bool value = SocketUtil.Write(socketClient, data);
+                if (!value)
+                {
+                    OnWriteError();
+                }
+                //try
+                //{
+                //    if (socketClient.Connected == true)
+                //    {
+                //        int send = socketClient.Send(data);
+                //    }
+                //}
+                //catch (SocketException e)
+                //{
+                //    throw (e);
+                //}
+
+            }
+        }
+    }
+
+    public int getLocalPort()
+    {
+        int ret = 0;
+        if (socketClient != null
+            && socketClient.LocalEndPoint != null)
+        {
+            int.TryParse(socketClient.LocalEndPoint.ToString().Split(':')[1], out ret);
+        }
+        return ret;
+    }
+
+    /// <summary>
+    /// 接收数据线程
+    /// </summary>
+    public void Receive()
+    {
+        while (socketClient != null
+            && socketClient.Connected == true)
+        {
+            if (socketClient.Poll(-1, SelectMode.SelectRead))
+            {
+                int len = SocketUtil.ReadLength(socketClient);
+                if (len > 0)
+                {
+                    int protocol = SocketUtil.ReadProtocol(socketClient);
+                    if (protocol > 0)
+                    {
+                        byte[] data = new byte[len];
+                        bool read = SocketUtil.ReadContent(socketClient, buffer, data);
+                        if (read)
+                        {
+                            string strReceive = Encoding.Default.GetString(data);
+                            queueRead.Enqueue(strReceive);
+                            //Debug.Log("client read from server:" + socketClient.RemoteEndPoint + "|" + strReceive);
+                        }
+                        else
+                        {
+                            OnReadContenError();
+                        }
+                    }
+                    else
+                    {
+                        OnReadProtocolError();
+                    }
+                }
+                else
+                {
+                    OnReadLengthError();
+                }
+            }
+
+        }
+    }
+
+    void startHeartBreakCheck()
+    {
+        Thread thread = new Thread(heartBreakCheck);
+        thread.IsBackground = true;
+        thread.Start();
+    }
+
+    void startHeartBreakSend()
+    {
+        timerSend = new System.Timers.Timer();
+        timerSend.Elapsed += new ElapsedEventHandler(onHeartBreakSend);
+        timerSend.Interval = 10000;
+        timerSend.AutoReset = true;
+        timerSend.Enabled = true;
+        timerSend.Start();
+    }
+
+    void heartBreakCheck()
+    {
+        Thread.Sleep(10000);
+        timerCheck = new System.Timers.Timer();
+        timerCheck.Elapsed += new ElapsedEventHandler(onHeartBreakCheck);
+        timerCheck.Interval = 20000;
+        timerCheck.AutoReset = true;
+        timerCheck.Enabled = true;
+        timerCheck.Start();
+    }
+
+    void onHeartBreakCheck(object source, ElapsedEventArgs e)
+    {
+        if (!heartBreak)
+        {
+            if (socketClient != null)
+            {
+                socketClient.Close();
+                socketClient = null;
+            }
+        }
+        else
+        {
+            heartBreak = false;
+        }
+    }
+
+    void onHeartBreakSend(object source, ElapsedEventArgs e)
+    {
+        ClientWrite(Protocol.HEART_BREAK, "1");
+    }
+
+    void OnReadLengthError()
+    {
+        queueLog.Enqueue("读取长度错误");
+    }
+
+    void OnReadProtocolError()
+    {
+        queueLog.Enqueue("读取协议错误");
+    }
+
+
+    void OnReadContenError()
+    {
+        queueLog.Enqueue("读取内容错误");
+    }
+
+    void OnWriteError()
+    {
+        queueLog.Enqueue("写错错误");
+    }
+}
---
+++ Socket/Assets/Script/SocketUtil.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class SocketUtil
+{
+
+    public static byte[] ConvertStrToSend(int protocol, string s)
+    {
+        try
+        {
+            byte[] content = Encoding.Default.GetBytes(s);
+            byte[] data = ConvertByteArrayToSend(protocol, content);
+            return data;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    /// <summary>
+    /// 读取内容长度
+    /// </summary>
+    /// <param name="soket"></param>
+    /// <returns></returns>
+    public static int ReadLength(Socket soket)
+    {
+        int ret = 0;
+        try
+        {
+            byte[] data = new byte[4];
+            ret = soket.Receive(data, 0, 4, SocketFlags.None);
+            if (ret > 0)
+            {
+                if (BitConverter.IsLittleEndian) // 若为 小端模式
+                {
+                    Array.Reverse(data); // 转换为 大端模式               
+                }
+                return BitConverter.ToInt32(data, 0);
+            }
+        }
+        catch (SocketException exception)
+        {
+            throw (exception);
+        }
+        return ret;
+    }
+
+    /// <summary>
+    /// 读取协议
+    /// </summary>
+    /// <param name="soket"></param>
+    /// <returns></returns>
+    public static int ReadProtocol(Socket soket)
+    {
+        int ret = 0;
+        try
+        {
+            byte[] data = new byte[4];
+            ret = soket.Receive(data, 0, 4, SocketFlags.None);
+            if (ret > 0)
+            {
+                if (BitConverter.IsLittleEndian) // 若为 小端模式
+                {
+                    Array.Reverse(data); // 转换为 大端模式               
+                }
+                return BitConverter.ToInt32(data, 0);
+            }
+        }
+        catch (SocketException exception)
+        {
+            throw (exception);
+        }
+        return ret;
+    }
+
+    /// <summary>
+    /// 读取内容
+    /// </summary>
+    /// <param name="soket"></param>
+    /// <returns></returns>
+    public static bool ReadContent(Socket soket, byte[] buffer, byte[] data)
+    {
+        try
+        {
+            int read = 0;
+            int length = data.Length;
+            int left = data.Length;
+            while (length > 0
+                && left > 0)
+            {
+                read = soket.Receive(buffer, 8 + read, left, SocketFlags.None);
+
+                if (read <= 0)
+                {
+                    return false;
+                }
+
+                if (read > 0)
+                {
+                    read += read;
+                    left -= read;
+                }
+
+                if (left <= 0)
+                {
+                    Buffer.BlockCopy(buffer, 0, data, 0, length);
+                    if (BitConverter.IsLittleEndian) // 若为 小端模式
+                    {
+                        Array.Reverse(data); // 转换为 大端模式               
+                    }
+                    return true;
+                }
+            }
+        }
+        catch (SocketException exception)
+        {
+            throw (exception);
+        }
+        return false;
+    }
+
+
+
+
+    /// <summary>
+    /// 读取内容长度
+    /// </summary>
+    /// <param name="soket"></param>
+    /// <returns></returns>
+    public static bool Write(Socket soket, byte[] data)
+    {
+        try
+        {
+            int send = 0;
+            int length = data.Length;
+            int left = data.Length;
+            while (length > 0
+                && left > 0)
+            {
+                send = soket.Send(data, send, left, SocketFlags.None);
+
+                if (send <= 0)
+                {
+                    return false;
+                }
+
+                if (send > 0)
+                {
+                    send += send;
+                    left -= send;
+                }
+
+                if (left <= 0)
+                {
+                    return true;
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            throw (e);
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 创建发送内容
+    /// </summary>
+    /// <param name="protocol"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public static byte[] ConvertByteArrayToSend(int protocol, byte[] content)
+    {
+        try
+        {
+            byte[] length = BitConverter.GetBytes(content.Length);
+            byte[] cmd = BitConverter.GetBytes(protocol);
+            byte[] data = new byte[content.Length + 8];
+            Buffer.BlockCopy(length, 0, data, 0, length.Length);//前4位代表内容长度
+            Buffer.BlockCopy(cmd, 0, data, 4, cmd.Length);//中间4位代表协议
+            Buffer.BlockCopy(content, 0, data, 8, content.Length);//内容
+            return data;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+
+    }
+
+    public static byte[] convertPositionData(int player, int posX, int posY)
+    {
+        try
+        {
+            byte[] p = BitConverter.GetBytes(player);
+            byte[] x = BitConverter.GetBytes(posX);
+            byte[] y = BitConverter.GetBytes(posY);
+            byte[] data = new byte[12];
+            Buffer.BlockCopy(p, 0, data, 0, p.Length);
+            Buffer.BlockCopy(x, 0, data, 4, x.Length);
+            Buffer.BlockCopy(y, 0, data, 8, y.Length);
+            return data;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static int readPlayer(byte[] data)
+    {
+        try
+        {
+            int ret = 0;
+            byte[] p = new byte[4];
+            Buffer.BlockCopy(data, 0, p, 0, p.Length);
+            ret = BitConverter.ToInt32(p, 0);
+            return ret;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static int readPosX(byte[] data)
+    {
+        try
+        {
+            int ret = 0;
+            byte[] x = new byte[4];
+            Buffer.BlockCopy(data, 4, x, 0, x.Length);
+            ret = BitConverter.ToInt32(x, 0);
+            return ret;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static int readPosY(byte[] data)
+    {
+        try
+        {
+            int ret = 0;
+            byte[] y = new byte[4];
+            Buffer.BlockCopy(data, 8, y, 0, y.Length);
+            ret = BitConverter.ToInt32(y, 0);
+            return ret;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static int[] readPositonData(byte[] data)
+    {
+        try
+        {
+            int[] ret = new int[3];
+            byte[] p = new byte[4];
+            byte[] x = new byte[4];
+            byte[] y = new byte[4];
+            Buffer.BlockCopy(data, 0, data, 0, p.Length);
+            Buffer.BlockCopy(data, 4, data, 0, x.Length);
+            Buffer.BlockCopy(data, 8, data, 0, y.Length);
+            ret[0] = BitConverter.ToInt32(p, 0);
+            ret[1] = BitConverter.ToInt32(x, 0);
+            ret[2] = BitConverter.ToInt32(y, 0);
+            return ret;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static byte[] readContent(byte[] buffer)
+    {
+        try
+        {
+            byte[] length = new byte[4];
+            Buffer.BlockCopy(buffer, 0, length, 0, 4);
+            int num = BitConverter.ToInt32(length, 0);
+            byte[] data = new byte[num];
+            Buffer.BlockCopy(buffer, 8, data, 0, num);
+            return data;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static byte[] readOriginalByteArray(byte[] buffer)
+    {
+        try
+        {
+            byte[] length = new byte[4];
+            Buffer.BlockCopy(buffer, 0, length, 0, 4);
+            int num = BitConverter.ToInt32(length, 0);
+            byte[] data = new byte[num + 8];
+            Buffer.BlockCopy(buffer, 0, data, 0, data.Length);
+            return data;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+
+    public static int readProtocol(byte[] buffer)
+    {
+        try
+        {
+            byte[] cmd = new byte[4];
+            Buffer.BlockCopy(buffer, 4, cmd, 0, 4);
+            int num = BitConverter.ToInt32(cmd, 0);
+            return num;
+        }
+        catch (Exception e)
+        {
+            throw (e);
+        }
+    }
+}
